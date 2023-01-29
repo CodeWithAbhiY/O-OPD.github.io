560fda0d5451b24f4ffb527ae15348d23075e7c8
CodeWithAbhiY <abhishek22april2002@gmail.com>
2023-01-29 22:12:41 +0530

    Online Out Patient Department

---
+++ script/scripts.js
@@ -0,0 +1,9 @@
+function onClickMenu(){
+    document.getElementById("menu").classList.toggle("icon");
+    document.getElementById("nav").classList.toggle("change");
+}
+function onClickUserLogo(){
+    document.getElementById("logdetails").classList.toggle("user-logoChange");
+    document.getElementById("user-logo").classList.toggle("user-logoColorChange");
+
+}
