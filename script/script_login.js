57e8e7b773ba67442e5d69d3943d036ff59cac7e
Abhishek Yadav <94221773+CodeWithAbhiY@users.noreply.github.com>
2023-04-10 01:18:28 +0530

    Delete script_login.js

--- script/script_login.js
+++
@@ -1,5 +0,0 @@
-const accountSid = "AC8184dbd3b13d3647a0d3d45aba7ea847";
-const authToken = "d2c5eab03fae742d43557cb136209a4e";
-const verifySid = "VAcf29cc318104b4bdd6d801877993bcd1";
-
-const client = require("twilio")(accountSid, authToken);
